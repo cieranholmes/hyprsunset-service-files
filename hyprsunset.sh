16cf42b3f31c4cb08920c50337c303de49892711
Cieran Holmes <holmescieran@gmail.com>
2025-04-28 11:47:06 +0200

    Fix timer syntax

--- hyprsunset.sh
+++ hyprsunset.sh
@@ -12,7 +12,7 @@ echo "Current hour is $current_hour"
 
 # If it’s 6pm to 7am, hyprsunset -t 5000
 # Else hyprsunset -t 6500
-if [ "$current_hour" -ge 19 ] || [ "$current_hour" -le 6 ]; then
+if [ "$current_hour" -ge 18 ] || [ "$current_hour" -le 6 ]; then
   echo "Changing to nighttime hyprsunset"
   hyprsunset -t 3400
 else
